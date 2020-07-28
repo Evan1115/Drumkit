e4199c0709b36808f8eaeb2dd1e7df33c582fc15
Joram <jing111597@gmail.com>
2020-07-28 23:12:19 +0800

    added eventlistener

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("hello");
+
+var drumButton = document.querySelectorAll("button");
+for(var i = 0 ; i < drumButton.length ; i++){
+    drumButton[i].addEventListener("click", function(){
+      alert("hello");
+    });
+}
