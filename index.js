fa6c73762a235aa1bdf8342dd66972450cf8b282
Joram <jing111597@gmail.com>
2020-08-01 23:58:37 +0800

    add sounds

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 
-var drumButton = document.querySelectorAll("button");
+var drumButton = document.querySelectorAll(".drum");
+
 for(var i = 0 ; i < drumButton.length ; i++){
     drumButton[i].addEventListener("click", function(){
-      alert("hello");
+      
+        var buttonInnerHTML = this.innerHTML;
+       makeSounds(buttonInnerHTML);
+      
     });
+
+    
+}
+
+//detect keyboard press
+
+document.addEventListener("keydown", function(event){ //pass in the event that trigger the keypress
+         
+         makeSounds(event.key);
+});
+
+function makeSounds(key){
+    
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+         case "a":
+          var kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+          break;
+          case "s":
+              var snare = new Audio("sounds/snare.mp3");
+              snare.play();
+          break;
+          case "d":
+              var tom1 = new Audio("sounds/tom-1.mp3");
+              tom1.play();
+          
+          break;
+          case "j":
+              var tom2 = new Audio("sounds/tom-2.mp3");
+              tom2.play();
+          break;
+          case "k":
+              var tom3 = new Audio("sounds/tom-3.mp3");
+              tom3.play();
+          break;
+          case "l":
+              var tom4 = new Audio("sounds/tom-4.mp3");
+              tom4.play();
+          break;
+        default:
+            console.log()
+            break;
+    }
 }
